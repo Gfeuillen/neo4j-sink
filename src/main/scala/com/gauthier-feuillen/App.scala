f5cbb38d297687aebe61c4ea71841b95b00ef538
gfeuillen <feuillen.gauthier@gmail.com>
2017-08-11 15:22:54 +0200

    working app

--- src/main/scala/com/gauthier-feuillen/App.scala
+++ src/main/scala/com/gauthier-feuillen/App.scala
@@ -1,15 +1,13 @@
-package com.gauthier-feuillen
+package com.gauthier.feuillen
 
-/**
- * @author ${user.name}
- */
+
 object App {
   
   def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
   
   def main(args : Array[String]) {
     println( "Hello World!" )
-    println("concat arguments = " + foo(args))
+    //println("concat arguments = " + foo(args))
   }
 
 }
